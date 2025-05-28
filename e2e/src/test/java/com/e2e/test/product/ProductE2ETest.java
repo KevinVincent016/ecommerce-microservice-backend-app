efc84d246402b13ed751af3c6caf8479d1d13aff
Kevin Vincent Loachamin Almeida <kevinvincentvl016@gmail.com>
2025-05-28 09:23:47 -0500

    Add E2E Test

---
+++ e2e/src/test/java/com/e2e/test/product/ProductE2ETest.java
@@ -0,0 +1,42 @@
+
+package com.e2e.test.product;
+
+import com.e2e.test.util.E2ESuite;
+import com.e2e.test.util.TestRestFacade;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+import static org.awaitility.Awaitility.await;
+import static org.testcontainers.shaded.org.awaitility.Durations.FIVE_SECONDS;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import java.util.Map;
+@SpringBootTest(classes = E2ESuite.class, webEnvironment = SpringBootTest.WebEnvironment.NONE)
+public class ProductE2ETest extends E2ESuite {
+
+    //@Autowired
+    //private TestRestTemplate testRestTemplate;
+    @Autowired
+    private TestRestFacade restFacade; // Autowire your facade
+
+    @Value("${product.service.url}") // This property is set by E2ESuite.Initializer
+    private String productServiceUrl;
+
+//    @Value("${user.service.url}") // This property is set by E2ESuite.Initializer
+//    private String userServiceUrl;
+
+    @Test
+    void shouldGetAllProducts() {
+        ResponseEntity<String> response = restFacade.get(productServiceUrl + "/product-service/api/categories", String.class);
+//        System.out.println("Response: " + response.getBody());
+//        System.out.println("Status Code: " + response.getStatusCode());
+        assertTrue(response.getStatusCode().is2xxSuccessful(), "Unexpected status code: " + response.getStatusCode());
+    }
+}
